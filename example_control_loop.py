e597603cce84ee452a4d37c8a44d764101f742de
Maximiliannnns <78545329+Maximiliannnns@users.noreply.github.com>
2022-04-18 21:48:14 +0300

    Update example_control_loop.py

--- example_control_loop.py
+++ example_control_loop.py
@@ -1,27 +1,3 @@
-#!/usr/bin/env python
-# Copyright (c) 2016, Universal Robots A/S,
-# All rights reserved.
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions are met:
-#    * Redistributions of source code must retain the above copyright
-#      notice, this list of conditions and the following disclaimer.
-#    * Redistributions in binary form must reproduce the above copyright
-#      notice, this list of conditions and the following disclaimer in the
-#      documentation and/or other materials provided with the distribution.
-#    * Neither the name of the Universal Robots A/S nor the names of its
-#      contributors may be used to endorse or promote products derived
-#      from this software without specific prior written permission.
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
-# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-# DISCLAIMED. IN NO EVENT SHALL UNIVERSAL ROBOTS A/S BE LIABLE FOR ANY
-# DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
-# ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-
 import sys
 sys.path.append('..')
 import logging
@@ -34,8 +10,7 @@ import math
 from pypylon import pylon
 from threading import Thread
 
-#logging.basicConfig(level=logging.INFO)
-
+#данные сервера робота
 ROBOT_HOST = '192.168.1.18'
 ROBOT_PORT = 30004
 config_filename = 'control_loop_configuration.xml'
@@ -52,17 +27,12 @@ watchdog_names, watchdog_types = conf.get_recipe('watchdog')
 con = rtde.RTDE(ROBOT_HOST, ROBOT_PORT)
 con.connect()
 
-# get controller version
 con.get_controller_version()
 
-# setup recipes
 con.send_output_setup(state_names, state_types)
 setp = con.send_input_setup(setp_names, setp_types)
 watchdog = con.send_input_setup(watchdog_names, watchdog_types)
 
-# Setpoints to move the robot to
-setp1 = [-0.12, -0.43, 0.1, 0, 3.11, 0.04]
-setp2 = [-0.12, -0.51, 0.1, 0, 3.11, 0.04]
 
 setp.input_double_register_0 = 0
 setp.input_double_register_1 = 0
@@ -71,28 +41,30 @@ setp.input_double_register_3 = 0
 setp.input_double_register_4 = 0
 setp.input_double_register_5 = 0
 
-# The function "rtde_set_watchdog" in the "rtde_control_loop.urp" creates a 1 Hz watchdog
 watchdog.input_int_register_0 = 0
 
+#расстояние площадки в пикселях
 w_px = 750
 w_mm = 440
 h_px = 500
 h_mm = 285
+#перевод в милимметры
 pixel_mm1 = (w_mm / w_px)
 pixel_mm2 = (h_mm / h_px)
 
 color_blue = (255, 0, 0)
 color_yellow = (0, 255, 255)
 
-#коэфеценты для исправления искажения изображения
+#находим искажение камеры с помощью прогарммы "find_undistort.py"
 dist_coef = np.array([[-1.51948828e+00,  1.95329695e+02, -1.04202010e-02, -3.03130271e-03,
-  -7.54902603e+03]])
-#camera_matrix = np.array([[640, 0, 480], [0, 640, 480], [0, 0, 1]])
+                      -7.54902603e+03]])
+
 camera_matrix = np.array([[3.32555825e+03, 0.00000000e+00, 9.49095102e+02],
- [0.00000000e+00, 3.20972678e+03, 5.61755191e+02],
- [0.00000000e+00, 0.00000000e+00, 1.00000000e+00]])
+                          [0.00000000e+00, 3.20972678e+03, 5.61755191e+02],
+                          [0.00000000e+00, 0.00000000e+00, 1.00000000e+00]])
 
 setp1 = [0, 0, 0, 0, 0, 0]
+
 
 def get_miny_point(box):
     ymin = box[0]
@@ -100,6 +72,7 @@ def get_miny_point(box):
         if p[1] < ymin[1]:
             ymin = p
     return ymin
+
 
 def get_minx_point(box):
     xmin = box[0]
@@ -109,6 +82,7 @@ def get_minx_point(box):
     return xmin
 
 
+#функция поиска кубика
 def find_box_list(img_original):
 
     setp1 = [0, 0, 0, 0, 0, 0]  # default if countours0 = 0
@@ -145,7 +119,7 @@ def find_box_list(img_original):
                 (reference[0] * usedEdge[0] + reference[1] * usedEdge[1]) / (
                         cv2.norm(reference) * cv2.norm(usedEdge))))
             angle1 = (math.pi * angle) / 180
-
+            #передача координат и угла поворота в точку перемещения
             setp1 = [Xm, Ym, 0.14, 0, 3.14, angle1]
 
             if area > 500:
@@ -153,18 +127,20 @@ def find_box_list(img_original):
                 cv2.circle(img_original, center, 5, color_yellow, 2)  # рисуем маленький кружок в центре прямоугольника
                 # выводим в кадр величину угла наклона
                 cv2.putText(img_original, "%d" % int(angle), (center[0] + 20, center[1] - 20),
-                           cv2.FONT_HERSHEY_SIMPLEX, 1, color_yellow, 2)
+                            cv2.FONT_HERSHEY_SIMPLEX, 1, color_yellow, 2)
 
     return img_original, setp1
 
+
 def setp_to_list(setp):
     list = []
-    for i in range(0,6):
+    for i in range(0, 6):
         list.append(setp.__dict__["input_double_register_%i" % i])
     return list
 
+
 def list_to_setp(setp, list):
-    for i in range (0,6):
+    for i in range(0, 6):
         setp.__dict__["input_double_register_%i" % i] = list[i]
     return setp
 
@@ -183,6 +159,7 @@ converter.OutputBitAlignment = pylon.OutputBitAlignment_MsbAligned
 if not con.send_start():
     sys.exit()
 
+#Освновной рабочий цикл в котором используются готовые функции
 while camera.IsGrabbing() and keep_running:
 
     state = con.receive()
@@ -200,26 +177,23 @@ while camera.IsGrabbing() and keep_running:
         img_undist = img_undist[450:950, 550:1300]
         result_img, setp1 = find_box_list(img_undist)
 
+        #если кубик найден - то передать координаты
         if state.output_int_register_0 != 0:
             list_to_setp(setp, setp1)
             print("send")
-            # send new setpoint
             con.send(setp)
 
+        #вывод видео на экран
         cv2.namedWindow('title', cv2.WINDOW_NORMAL)
         cv2.imshow('title', result_img)
         k = cv2.waitKey(1)
         if k == 27:
             break
 
-        # kick watchdog
         con.send(watchdog)
 
     grabResult.Release()
 
-    # Releasing the resource
 camera.StopGrabbing()
-
 con.send_pause()
-
 con.disconnect()
